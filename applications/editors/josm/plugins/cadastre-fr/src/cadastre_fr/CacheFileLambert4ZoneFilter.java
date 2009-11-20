cd80e1f1f6c56ba44f960e08d442f0f501175d74
pieren <pieren3@gmail.com>
2009-11-20 23:19:58 +0000

    Add subprojection handling

--- applications/editors/josm/plugins/cadastre-fr/src/cadastre_fr/CacheFileLambert4ZoneFilter.java
+++ applications/editors/josm/plugins/cadastre-fr/src/cadastre_fr/CacheFileLambert4ZoneFilter.java
@@ -5,7 +5,7 @@ import static org.openstreetmap.josm.tools.I18n.tr;
 import java.io.File;
 import javax.swing.filechooser.FileFilter;
 
-public class CacheFileFilter extends FileFilter {
+public class CacheFileLambert4ZoneFilter extends FileFilter {
 
     /**
      * Derived from ExtensionFileFilter writen by imi
@@ -13,18 +13,18 @@ public class CacheFileFilter extends FileFilter {
     private final String extension;
     private final String description;
 
-    public static CacheFileFilter[] filters = {
-        new CacheFileFilter("1", tr("Lambert Zone 1 cache file (.1)")),
-        new CacheFileFilter("2", tr("Lambert Zone 2 cache file (.2)")),
-        new CacheFileFilter("3", tr("Lambert Zone 3 cache file (.3)")),
-        new CacheFileFilter("4", tr("Lambert Zone 4 cache file (.4)"))
+    public static CacheFileLambert4ZoneFilter[] filters = {
+        new CacheFileLambert4ZoneFilter("1", tr("Lambert Zone 1 cache file (.1)")),
+        new CacheFileLambert4ZoneFilter("2", tr("Lambert Zone 2 cache file (.2)")),
+        new CacheFileLambert4ZoneFilter("3", tr("Lambert Zone 3 cache file (.3)")),
+        new CacheFileLambert4ZoneFilter("4", tr("Lambert Zone 4 cache file (.4)"))
         };
 
     /**
      * Construct an extension file filter by giving the extension to check after.
      *
      */
-    private CacheFileFilter(String extension, String description) {
+    private CacheFileLambert4ZoneFilter(String extension, String description) {
         this.extension = extension;
         this.description = description;
     }
