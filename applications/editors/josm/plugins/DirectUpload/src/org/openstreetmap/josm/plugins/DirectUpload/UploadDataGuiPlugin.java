e228ff3697a52dca5c664de0f1fe2f88829e31b7
Martin Garbe <leo@running-sheep.com>
2010-04-11 09:02:28 +0000

    fixed ticket #4782. Activate Upload if one GPX layer available.

--- applications/editors/josm/plugins/DirectUpload/src/org/openstreetmap/josm/plugins/DirectUpload/UploadDataGuiPlugin.java
+++ applications/editors/josm/plugins/DirectUpload/src/org/openstreetmap/josm/plugins/DirectUpload/UploadDataGuiPlugin.java
@@ -10,6 +10,7 @@ import static org.openstreetmap.josm.tools.I18n.tr;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
+import java.util.List;
 
 import org.openstreetmap.josm.Main;
 import org.openstreetmap.josm.actions.JosmAction;
@@ -43,6 +44,7 @@ public class UploadDataGuiPlugin extends Plugin{
 
         @Override
         protected void updateEnabledState() {
+            // enable button if there is "one active GpxLayer" or "exactly one GpxLayer in the list of all layers available"
             if(Main.map == null
                     || Main.map.mapView == null
                     || Main.map.mapView.getActiveLayer() == null
@@ -50,6 +52,12 @@ public class UploadDataGuiPlugin extends Plugin{
                 setEnabled(false);
             } else {
                 setEnabled(true);
+            }
+
+            if(Main.map != null && Main.map.mapView.getNumLayers() > 1) {
+                List<GpxLayer> list = Main.map.mapView.getLayersOfType(GpxLayer.class);
+                if (list.size() == 1)
+                    setEnabled(true);
             }
 
         }		
