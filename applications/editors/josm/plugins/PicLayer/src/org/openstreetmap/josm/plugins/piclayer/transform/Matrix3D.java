8bfb6e0bf67669894c3655f4181cf0f378e85a42
don-vip <vincent.privat@gmail.com>
2014-03-24 21:24:40 +0000

    [josm_piclayer] code cleanup, https link to josm website

--- applications/editors/josm/plugins/PicLayer/src/org/openstreetmap/josm/plugins/piclayer/transform/Matrix3D.java
+++ applications/editors/josm/plugins/PicLayer/src/org/openstreetmap/josm/plugins/piclayer/transform/Matrix3D.java
@@ -39,13 +39,13 @@ class Matrix3D {
                 for (int k=0; k<3; k++)
                     sum += a[i][k]*m.a[k][j];
                 result.a[i][j] = sum;
-            }				
+            }                
         return result;
     }
     
     private double determinant() {
         return a[0][0]*(a[1][1]*a[2][2]-a[1][2]*a[2][1])-a[0][1]*(a[1][0]*a[2][2]-a[1][2]*a[2][0])
-            +a[0][2]*(a[1][0]*a[2][1]-a[1][1]*a[2][0]);
+               +a[0][2]*(a[1][0]*a[2][1]-a[1][1]*a[2][0]);
     }
     
     public Matrix3D inverse() throws NoSolutionException {
