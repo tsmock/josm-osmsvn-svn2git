26776ff783b688cae9340dcf7df051fa93987ae6
don-vip <vincent.privat@gmail.com>
2016-07-02 02:06:14 +0000

    checkstyle

---
+++ applications/editors/josm/plugins/apache-commons/src/org/openstreetmap/josm/plugins/commons/CommonsPlugin.java
@@ -0,0 +1,16 @@
+// License: GPL. For details, see LICENSE file.
+package org.openstreetmap.josm.plugins.commons;
+
+import org.openstreetmap.josm.plugins.Plugin;
+import org.openstreetmap.josm.plugins.PluginInformation;
+
+public class CommonsPlugin extends Plugin {
+
+    /**
+     * Constructs a new {@code CommonsPlugin}.
+     * @param info plugin information
+     */
+    public CommonsPlugin(PluginInformation info) {
+        super(info);
+    } 
+}
