46e765191c1b051d8d4b1ced778d23f98ce3e13d
Taylor Smock <tsmock@meta.com>
2023-09-18 12:36:02 +0000

    Fix #19064: resolve all tools using Apache Ivy (JMapViewer)
    JUnit was updated from v4 to v5 as well.

--- applications/viewer/jmapviewer/test/org/openstreetmap/gui/jmapviewer/TileRangeTest.java
+++ applications/viewer/jmapviewer/test/org/openstreetmap/gui/jmapviewer/TileRangeTest.java
@@ -1,20 +1,21 @@
 // License: GPL. For details, see Readme.txt file.
 package org.openstreetmap.gui.jmapviewer;
 
-import static org.junit.Assert.assertEquals;
 
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
 
 /**
  * Unit tests of {@link TileRange} class.
  */
-public class TileRangeTest {
+class TileRangeTest {
 
     /**
      * Unit test of {@link TileRange#size}.
      */
     @Test
-    public void testSize() {
+    void testSize() {
         assertEquals(16, new TileRange(
                 new TileXY(3, 3), 
                 new TileXY(6, 6), 10).size());
