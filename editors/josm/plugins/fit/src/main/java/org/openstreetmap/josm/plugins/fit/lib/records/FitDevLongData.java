4a0e2460b279c1768cb8bfb91b63906f217eb49f
Taylor Smock <tsmock@meta.com>
2023-10-02 16:10:52 +0000

    Move fit to FIT to match expectations of the Nexus-JOSM-Plugins CI job

--- editors/josm/plugins/fit/src/main/java/org/openstreetmap/josm/plugins/fit/lib/records/FitDevLongData.java
+++
@@ -1,5 +0,0 @@
-// License: GPL. For details, see LICENSE file.
-package org.openstreetmap.josm.plugins.fit.lib.records;
-
-public record FitDevLongData(String fieldName, String unit, Long value) implements IFitDevData<Long> {
-}
