d7b1d2d0899965ecac1264029b15fdf931f7377f
Taylor Smock <tsmock@meta.com>
2023-10-02 16:10:52 +0000

    Move fit to FIT to match expectations of the Nexus-JOSM-Plugins CI job

--- applications/editors/josm/plugins/fit/src/main/java/org/openstreetmap/josm/plugins/fit/lib/records/FitDevIntData.java
+++
@@ -1,5 +0,0 @@
-// License: GPL. For details, see LICENSE file.
-package org.openstreetmap.josm.plugins.fit.lib.records;
-
-public record FitDevIntData(String fieldName, String unit, Integer value) implements IFitDevData<Integer> {
-}
